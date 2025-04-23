4f49f3f3203ab3aa516dd328d68934a27c562d69
quoca <quocanhtapcode@gmail.com>
2025-04-23 15:08:02 +0700

    Start folder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'BT thay Que',
+    version= '0.0.0',
+    author= 'Quoc Anh',
+    author_email= 'quocanh20102003@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
